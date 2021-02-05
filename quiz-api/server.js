826af1769692f7c96577c55e09b540e14f34c404
Liam Bradbury <liam.bradbury98@gmail.com>
2021-02-05 15:40:27 +0000

    back-end bcrupt needs work

--- quiz-api/server.js
+++ quiz-api/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require("body-parser")
+const bcrypt = require('bcrypt-nodejs');
 
-app.use(bodyParser.json())
 const app = express()
+app.use(bodyParser.json())
+let idCounter = 124;
 
- 
+
 const database = {
     users:[
         {
@@ -17,7 +19,7 @@ const database = {
             id:"124",
             name: "lily",
             email:"lily@gmail.com",
-            password: "toby123",
+            password: "lily123",
             score: 0
         }
     ]
@@ -30,18 +32,63 @@ app.get("/", (req , res)=>{
 
 
 app.post("/signin", (req, res)=>{
-    if(req.body.email === database.users[0].email){
-        res.json("email match!")
+    bcrypt.compare("bacon", hash, function(err, res) {
+        // res == true
+    });
+    bcrypt.compare("veggies", hash, function(err, res) {
+        // res = false
+    });
+    if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
+        res.json("email and password match a current user!")
     } else {
-        res.send("email doesnt match")
+        res.json("email and pass doesnt match a current user")
     }
-    if(req.body.password === database.users[0].password){
-        res.json("pass match")
-    } else {
-        res.send("password doesent match!")
-    }
-    res.send("signin")
+
 })
+
+
+app.post("/register", (req, res) =>{
+    const {email, name, password} = req.body
+    bcrypt.hash(password, null, null, function(err, hash) {
+        // Store hash in your password DB.
+    });
+    idCounter += 1
+    database.users.push({
+        id : idCounter.toString(),
+        name: "Dave",
+        email: "dave@gmail.com",
+        password: "dave123",
+        score: 0
+        
+
+    })
+    res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id', (req, res)=>{
+    const { id } = req.params;
+    let found = false;
+
+    database.users.forEach(user => {
+        if(user.id === id){
+            found = true;
+            return res.json(user);
+        }
+
+
+
+    })
+    if(!found){
+        res.status(400).json('not found');
+
+    }
+})
+
+
+
+
+
+
 
 
 app.listen(3000, ()=>{
